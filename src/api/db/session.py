0352340313f1603fe5486dd8039fdb61855a7cce
grimfurious-2801 <manthinidineshsairam@gmail.com>
2026-03-09 17:44:11 +0530

    connected to db

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -13,3 +13,12 @@ def init_db():
     print('creating database')
     SQLModel.metadata.create_all(engine)
 
+# from sqlalchemy import create_engine, text
+
+# CONNECTION = "timescaledb://tsdbadmin:n1lz9wz90m4avdiu@my2oashliv.mpor6xgixq.tsdb.cloud.timescale.com:36852/tsdb"
+
+# engine = create_engine(CONNECTION)
+# with engine.connect() as conn:
+#     cursor = conn.execute(text('select extname, extversion from pg_extension;'))
+#     for extension in cursor:
+#         print(extension)
